bcafb8cfe9f72dddd7340a7cf867e6da23ba754a
marcinkalandyk <marcin.kalandyk@gmail.com>
2017-09-27 14:22:25 +0200

    use databases

--- server-java/src/main/java/pl/kalandyk/suplement/selector/domain/HealthProblem.java
+++ server-java/src/main/java/pl/kalandyk/suplement/selector/domain/HealthProblem.java
@@ -1,15 +1,30 @@
 package pl.kalandyk.suplement.selector.domain;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
  * Created by marcinkalandyk on 02.05.2017.
  */
+
+@Entity
 public class HealthProblem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
     private String description;
     private String cause;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "HEALTHPROBLEM_SUPLEMENT",
+            joinColumns = @JoinColumn(name = "HEALTH_PROBLEM_ID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "SUPLEMENT_ID", referencedColumnName = "ID"))
 
     private List<Suplement> suplements;
 
@@ -17,9 +32,18 @@ public class HealthProblem {
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -27,6 +51,7 @@ public class HealthProblem {
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
@@ -34,6 +59,7 @@ public class HealthProblem {
     public String getCause() {
         return cause;
     }
+
     public void setCause(String cause) {
         this.cause = cause;
     }
