2b2b817e3882151f3360b65d323e6117a59ececc
Marcin Kalandyk <marcin.kalandyk@gmail.com>
2017-09-28 09:12:30 -0700

    clean up

--- UI-c/WpfBasics/WpfBasics/ViewModel/MainWindow.xaml.cs
+++ UI-c/WpfBasics/WpfBasics/ViewModel/MainWindow.xaml.cs
@@ -31,63 +31,7 @@ namespace WpfBasics
 
         public MainWindow()
         {
-            //HealthProblem healthProblem = new HealthProblem();
-            //healthProblem.Name = "Zespół przewlekłego zmęczenia";
-            //healthProblem.Description = "Zespół przewlekłego zmęczenia (ang. Chronic Fatigue Syndrome, CFS) obecnie jest uznawany za chorobę cywilizacyjną." +
-            //    " Cierpią na nią przede wszystkim młode, aktywne kobiety, pracujące zawodowo i opiekujące się dziećmi oraz domem. Zmęczenie polega na tym," +
-            //    " że uczucie wyczerpania towarzyszy przez kilka tygodni, mimo długiego wypoczynku. Chroniczne zmęczenie ogranicza aktywność człowieka o ponad 50%." +
-            //    " Objawy przewlekłego zmęczenia występują zarówno u osób zdrowych, jak i cierpiących na choroby somatyczne oraz niektóre zaburzenia psychiczne.";
-            //healthProblem.Cause = "Jakas przyczyna";
-
-            //Suplement suplement = new Suplement();
-            //suplement.Name = "Ashwagandha";
-            //suplement.ChosenSuplement = suplement.Name;
-            //suplement.SuplementDescription = "opis";
-            //suplement.HowTo = "3 razy dziennie po 600mg";
-            //suplement.LinkUrl = "www.muscle-zone.pl/ashwa";
-
-            //Suplement suplement1 = new Suplement();
-            //suplement1.Name = "Rhodiola";
-            //suplement1.ChosenSuplement = suplement1.Name;
-            //suplement1.SuplementDescription = "opis1";
-            //suplement1.HowTo = "3 razy dziennie po 500mg";
-            //suplement1.LinkUrl = "www.muscle-zone.pl/rhodiola";
-
-
-
-            //healthProblem.Suplements = new List<Suplement>(new Suplement[] { suplement, suplement1 });
-
-
-            ////TODO: DOdac obiekty
-
-            //HealthProblem healthProblem1 = new HealthProblem();
-            //healthProblem1.Name = "Słaby sen";
-            //healthProblem1.Description = "bla";
-            //healthProblem1.Cause = "abl";
-
-            //Suplement suplement3 = new Suplement();
-            //suplement3.Name = "5-HTP";
-            //suplement3.ChosenSuplement = suplement3.Name;
-            //suplement3.SuplementDescription = "opis3";
-            //suplement3.HowTo = "3 razy dziennie po 400mg";
-            //suplement3.LinkUrl = "www.muscle-zone.pl/5-htp";
-
-
-            //Suplement suplement4 = new Suplement();
-            //suplement4.Name = "GABA";
-            //suplement4.ChosenSuplement = suplement4.Name;
-            //suplement4.SuplementDescription = "opis4";
-            //suplement4.HowTo = "3 razy dziennie po 1 tabletce";
-            //suplement4.LinkUrl = "www.muscle-zone.pl/gaba";
-
-
-            //healthProblem1.Suplements = new List<Suplement>(new Suplement[] { suplement3, suplement4 });
-
             //http://restsharp.org/
-
-            //selectedItem = new SelectedItem();
-            //selectedItem.AllHealthProblems = new List<HealthProblem>(new HealthProblem[] { healthProblem, healthProblem1 });
-
             //zobaczenie ip do hosta - ipconfig /all
             RestClient restClient =  new RestClient("http://10.0.2.2:8080" /* adres serwera JAVovego, musi być włączony*/);
             RestRequest restRequest = new RestRequest("healthProblems", Method.GET);
@@ -111,26 +55,6 @@ namespace WpfBasics
         private void SuplementComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectedItem.SelectedSuplement = ((Suplement)((ComboBox)sender).SelectedValue);
-
-        }
-
-        private void OpisDolegliwosciTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void PrzyczynyTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-      
-        private void OpisSuplementuTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void StosowanieTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
         }
 
