d42ab2093e73057b774da3023a6bd0f7a5741467
Marcin Kalandyk <marcin.kalandyk@gmail.com>
2017-09-29 13:36:26 -0700

    add test

---
+++ UI-c/WpfBasics/WpfBasicsTests1/Data/DataLoaderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfBasics.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.IO;
+
+namespace WpfBasics.Data.Tests
+{
+    [TestClass()]
+    public class DataLoaderTests
+    {
+       
+
+        [TestMethod()]
+        public void DataLoaderTest()
+        {
+            Mock<IHealthProblemsRestClient> restMock = new Mock<IHealthProblemsRestClient>();
+
+            DataLoader dataLoader = new DataLoader((IHealthProblemsRestClient)restMock.Object);
+            restMock.Setup(x => x.LoadData()).Returns(@"");
+
+            var result = dataLoader.LoadData();
+
+
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
