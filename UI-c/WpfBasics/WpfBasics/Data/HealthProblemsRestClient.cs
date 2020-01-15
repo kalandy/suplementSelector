3caa964899c411eb7fdf5a5e0874899e72c60de1
kalandy <jamakasi3@gmail.com>
2020-01-15 13:09:00 +0000

    add server address

--- UI-c/WpfBasics/WpfBasics/Data/HealthProblemsRestClient.cs
+++ UI-c/WpfBasics/WpfBasics/Data/HealthProblemsRestClient.cs
@@ -16,12 +16,14 @@ namespace WpfBasics.Data
     {
         //http://restsharp.org/
         //zobaczenie ip do hosta - ipconfig /all
-        RestClient restClient = new RestClient("http://ec2-3-17-4-120.us-east-2.compute.amazonaws.com:8080" /* adres serwera JAVovego, musi być włączony*/);
+        // http://10.0.2.2:8080  adres serwera JAVovego, musi być włączony http://ec2-52-14-145-69.us-east-2.compute.amazonaws.com:8080/
+        RestClient restClient = new RestClient("http://ec2-52-14-145-69.us-east-2.compute.amazonaws.com:8080/");
         RestRequest restRequest = new RestRequest("healthProblems", Method.GET);
 
 
-        public string GetHealthProblemsJson() {   /*!< pobranie JSON'a */
-          
+        public string GetHealthProblemsJson()
+        {   /*!< pobranie JSON'a */
+
             return restClient.Execute(restRequest).Content;
         }
     }
